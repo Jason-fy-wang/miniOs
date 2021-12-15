e2b436efe06af89a2035f3131aaea5af918ac048
emperWang <544094478@qq.com>
2021-12-15 08:39:01 +0800

    print function

--- chapter-6/kernel/main.c
+++ chapter-6/kernel/main.c
@@ -2,16 +2,8 @@
 int main(void)
 {
     put_char('K');
-    put_char('e');
-    put_char('r');
-    put_char('n');
-    put_char('e');
-    put_char('l');
-    put_char('\n');
-    put_char('1');
-    put_char('2');
-    put_char('\b');
-    put_char('3');
+    put_str("i am kernel\n");
+    put_int(500);
     while (1)
     {
     }
--- chapter-6/lib/kernel/print.h
+++ chapter-6/lib/kernel/print.h
@@ -3,4 +3,6 @@
 
 #include "stdint.h"
 void put_char(uint8_t c);
+void put_str(char *message);
+void put_int(uint32_t num);
 #endif
