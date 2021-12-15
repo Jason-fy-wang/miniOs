12d7ae3829a52ffdd5cde833470db9d7d1f772d0
emperWang <544094478@qq.com>
2021-12-15 11:14:04 +0800

    fix format

--- chapter-6/kernel/main.c
+++ chapter-6/kernel/main.c
@@ -1,11 +1,9 @@
 #include "print.h"
-int main(void)
-{
-    put_char('K');
-    put_str("i am kernel\n");
-    put_int(500);
-    while (1)
-    {
-    }
-    return 0;
+int main(void) {
+  put_char('K');
+  put_str("i am kernel\n");
+  put_int(500);
+  while (1) {
+  }
+  return 0;
 }
