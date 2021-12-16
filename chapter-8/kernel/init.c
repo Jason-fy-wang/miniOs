02c6caf5f823530404e7fbe4c9713446dbf5454b
emperWang <544094478@qq.com>
2021-12-16 23:36:28 +0800

    base memory manager

--- chapter-8/kernel/init.c
+++ chapter-8/kernel/init.c
@@ -2,10 +2,12 @@
 #include "print.h"
 #include "interrupt.h"
 #include "timer.h"
+#include "memory.h"
 
 void init_all(){
     put_str("init_all \n");
-    idt_init();
-    timer_init();
+    //idt_init();
+    //timer_init();
+    mem_init();
 }
 
