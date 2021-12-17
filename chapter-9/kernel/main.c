de2814a104d9305d1ddb9c561cd323a0c39a8492
emperWang <544094478@qq.com>
2021-12-17 17:07:13 +0800

    base thread

--- chapter-9/kernel/main.c
+++ chapter-9/kernel/main.c
@@ -2,6 +2,9 @@
 #include "init.h"
 #include "debug.h"
 #include "memory.h"
+#include "thread.h"
+
+void k_thread_a(void *);
 
 int main(void){
     put_str("i am kernel\n");
@@ -11,6 +14,14 @@ int main(void){
     put_str("\n get_kernel_page start vaddr is : ");
     put_int((uint32_t)(vaddr));
     put_str("\n");
+    thread_start("threadA", 31, k_thread_a,"argA");
     while (1);
     return 0;
+}
+
+void k_thread_a(void *arg_){
+    char * arg = arg_;
+    while(1){
+        put_str(arg);
+    }
 }
