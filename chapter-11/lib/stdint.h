5b910b5871700b6df7a87185ce9bd4a2df812da3
emperWang <544094478@qq.com>
2021-12-21 09:30:12 +0800

    fix uint16_t

--- chapter-11/lib/stdint.h
+++ chapter-11/lib/stdint.h
@@ -6,7 +6,7 @@ typedef signed int int32_t;
 typedef signed long long int int64_t;
 
 typedef unsigned char uint8_t;
-typedef unsigned short uint16_t;
+typedef unsigned short int uint16_t;
 typedef unsigned int uint32_t;
 typedef unsigned long long int uint64_t;
 
