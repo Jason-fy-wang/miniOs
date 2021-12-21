0feb7ac5cc27df76ada0cd0287086dd1063954ab
emperWang <544094478@qq.com>
2021-12-21 09:29:34 +0800

    __FUNCTION__ --> __func__

--- chapter-11/kernel/debug.h
+++ chapter-11/kernel/debug.h
@@ -3,13 +3,13 @@
 
 void panic_spin(char *filename, int line, const char* func, const char* condition);
 
-#define PANIC(...) panic_spin(__FILE__, __LINE__, __FUNCTION__,__VA_ARGS__)
+#define PANIC(...) panic_spin(__FILE__, __LINE__, __func__,__VA_ARGS__)
 
 #ifdef NDEBUG
-    #define ASSERT(CONSITION) (void(0))
+    #define ASSERT(CONDITION) ((void)0)
 #else
-    #define ASSERT(CONSITION) \
-    if(CONSITION){}else {PANIC(#CONSITION);}
+    #define ASSERT(CONDITION) \
+    if(CONDITION){}else {PANIC(#CONDITION);}
 #endif /*NDEBUG*/
 
 #endif /*_KERNEL_DEBUG_H_*/
